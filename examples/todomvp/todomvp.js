37c919233ce1ed692f3510cc916ccdc2af8143a7
ohanhi <ossi.hanhinen@gmail.com>
2015-06-28 22:00:25 +0300

    Updating the read menu

--- examples/todomvp/todomvp.js
+++ examples/todomvp/todomvp.js
@@ -7,7 +7,7 @@ funker.setupFunk();
 // JS6 classes make everything fitter, better and happier!
 class Optimus {
   constructor (feat) {
-    feat !== 'Prime' && throw new TypeError('Must be Prime.');
+    feat != 'Prime' && throw new TypeError('Must be Prime.');
     this.feat = 'Prime';
   }
   get feat () {
